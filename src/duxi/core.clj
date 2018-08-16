1d987c86c23c1f9144f8737f8582c2141b2a7af7
Henry Garner <henry@henrygarner.com>
2018-08-16 15:56:11 +0100

    Refactor and rename

--- src/duxi/core.clj
+++ src/duxi/core.clj
@@ -3,23 +3,37 @@
             [duxi.rfr :refer [>seq]]
             [kixi.stats.core :as kixi]
             [redux.core :as redux])
-  (:refer-clojure :exclude [juxt identity]))
+  (:refer-clojure :exclude [juxt identity transduce]))
 
-(defprotocol IDuct
-  (unduct [this] "Returns the Duct"))
+(deftype Unfinished [thing]
+  clojure.lang.IDeref
+  (deref [this] thing))
 
-(deftype Duct [thing]
-  IDuct
-  (unduct [this] thing))
+(defn unfinished?
+  "Is there more to do?"
+  [x]
+  (instance? Unfinished x))
+
+(defn unfinished
+  "Mark a return value as a pending computation"
+  [x]
+  (Unfinished. x))
+
+(defn transduce
+  "Like clojure.core/transduce, but will short-cut a reduced? init value"
+  ([xform f coll] (transduce xform f (f) coll))
+  ([xform f init coll]
+   (cond (reduced? init) (unreduced init)
+         :else (clojure.core/transduce xform f init coll))))
 
 (defn conduct
   "Simples"
-  [rfr coll]
-  (loop [rfr rfr]
-    (let [res (transduce clojure.core/identity (rfr coll) coll)]
-      (if (satisfies? IDuct res)
-        (recur (unduct res))
-        res))))
+  [reductor coll]
+  (loop [rdtr reductor]
+    (let [result (transduce clojure.core/identity (rdtr coll) coll)]
+      (if (unfinished? result)
+        (recur @result)
+        result))))
 
 (defn reductor
   "Transform a reducing function into a reductor.
@@ -32,17 +46,16 @@
 
 (defn xform
   "Wraps a reductor in a transducer"
-  [xf rdtr]
+  [xf reductor]
   (fn [coll]
-    (xf (rdtr coll))))
+    (xf (reductor coll))))
 
 (defn constant
   "A reducing function which returns a constant value"
   [val]
   (completing
    (fn
-     ([] nil)
-     ([_ _] (reduced val)))))
+     ([] (reduced val)))))
 
 (defn identity
   "A reductor which returns a constant value"
@@ -50,51 +63,49 @@
   (fn [coll] (constant val)))
 
 (defn post-complete
+  "Chain a completing function after rf completion"
   [rf f]
   (completing rf #(f (rf %))))
 
+(defn post-reduce
+  "Chain a function accepting the return value of a reductor
+  to execute after the reductor has finished."
+  [reductor f]
+  (fn [coll]
+    (post-complete (reductor coll) f)))
+
 (defmonad duct-m
-  [m-bind (fn [mv f]
-            (fn [coll]
-              (post-complete (mv coll)
-                             (fn [acc]
-                               (Duct. (f acc))))))
+  [m-bind (fn [mv f] (post-reduce mv (comp unfinished f)))
    m-result identity])
 
 (defmacro letduct
+  "Create a reductor by chaining reductors together"
   [specs & body]
   `(domonad duct-m ~specs ~@body))
 
 (defmacro defduct
+  "Define a reductor by optionally chaining previous reductor steps"
   [name docstring specs & body]
   `(def ~name
      (letduct ~specs ~@body)))
 
-(defn duct?
-  [duct]
-  (satisfies? IDuct duct))
-
-(defn deduct
-  [de]
-  (cond-> de
-    (duct? de) unduct))
-
 (defn juxt
-  "A juxt which works for reductors"
-  [& rfrs]
+  "Takes a set of reductors and returns a reductor that is the
+  juxtaposition of those reductors. Returns a vector containing the
+  result of applying each reductor to the collection (left-to-right).
+  (conduct (juxt a b) coll) => [(conduct a coll) (conduct b coll)]"
+  [& reductors]
   (fn [coll]
-    (post-complete (apply redux/juxt (map #(% coll) rfrs))
-                   (fn [accs]
-                     (let [continue? (some duct? accs)
-                           rfrs (mapv (fn [acc]
-                                        (if (and continue? (not (duct? acc)))
-                                          (identity acc)
-                                          (deduct acc)))
-                                      accs)]
-                       (if continue?
-                         (Duct. (apply juxt rfrs))
-                         rfrs))))))
-
+    (post-complete (apply redux/juxt (map #(% coll) reductors))
+                   (fn [results]
+                     (if (some unfinished? results)
+                       (let [reductors (map (fn [result]
+                                              (if (unfinished? result)
+                                                (deref result)
+                                                (identity result)))
+                                            results)]
+                         (unfinished (apply juxt reductors)))
+                       results)))))
 
 ;; Example
 
@@ -115,7 +126,6 @@
 (conduct normalise [2 4 4 4 5 5 5 7 9])
 
 ;;=> [-1.5 -0.5 -0.5 -0.5 0.0 0.0 0.0 1.0 2.0]
-
 
 (conduct (juxt sum normalise) [2 4 4 4 5 5 5 7 9])
 
