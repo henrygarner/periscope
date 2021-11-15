fd5024a01cd1fa373cc0e27ba3560ce464f52c7f
Henry Garner <henry@henrygarner.com>
2021-11-15 14:50:32 +0000

    Move ducts to own namespace, add last

---
+++ src/ductors/ducts.clj
@@ -0,0 +1,119 @@
+(ns ductors.ducts
+  (:require [clojure.core :as core])
+  (:refer-clojure :exclude [vals key subseq map filter remove update get assoc]))
+
+(defn- thrush
+  [state f]
+  (f state))
+
+(defn- conj*
+  [handler]
+  (fn
+    ([] (conj))
+    ([acc] (conj acc))
+    ([acc x] (conj acc x))
+    ([acc x f] (conj acc (handler x f)))))
+
+(defn- transduce*
+  [f xf rf coll]
+  (let [rf (xf rf)]
+    (rf (reduce (fn [acc x]
+                  (rf acc x f))
+                (rf)
+                coll))))
+
+(defn duct
+  "A duct is a lens over a transducer"
+  [xf]
+  (fn [handler]
+    (fn
+      ([coll]
+       (let [xf (comp xf (core/map handler))]
+         (if (seq? coll)
+           (sequence xf coll)
+           (into (empty coll) xf coll))))
+      ([coll f]
+       (if (seq? coll)
+         (seq (transduce* f xf (conj* handler) coll))
+         (transduce* f xf (conj* handler) coll))))))
+
+(defn xform->lens
+  [xf f]
+  (fn [& args]
+    (fn [rf]
+      (let [rf' ((apply xf args) rf)]
+        (fn
+          ([] (rf'))
+          ([acc] (rf' acc))
+          ([acc x] (rf' acc x))
+          ([acc x f']
+           (apply f rf acc x f' args)))))))
+
+(def filter
+  (xform->lens core/filter
+               (fn [rf acc x f pred]
+                 (if (pred x)
+                   (rf acc x f)
+                   (rf acc x identity)))))
+
+(def map
+  (xform->lens core/map
+               (fn [rf acc x f fx]
+                 (rf acc (fx x) f))))
+
+(def remove (comp filter complement))
+
+(defn take [n]
+  (fn [rf]
+    (let [nv (volatile! n)]
+      (fn
+        ([] (rf))
+        ([result] (rf result))
+        ([result input]
+         (let [n @nv
+               nn (vswap! nv dec)
+               result (if (pos? n)
+                        (rf result input)
+                        result)]
+           (if (not (pos? nn))
+             (ensure-reduced result)
+             result)))
+        ([result input f]
+         (let [n @nv
+               nn (if (= f identity) n (vswap! nv dec))]
+           (if (pos? n)
+             (rf result input f)
+             (rf result input identity))))))))
+
+(def last
+  (fn [rf]
+    (let [last (volatile! nil)
+          buffer (volatile! [])
+          fx (volatile! nil)]
+      (fn
+        ([] (rf))
+        ([acc]
+         (let [acc (if @last
+                     (if @fx
+                       (rf acc @last @fx)
+                       (rf acc @last))
+                     acc)
+               acc (reduce (fn [acc x]
+                             (rf acc x identity))
+                           acc @buffer)]
+           (rf acc)))
+        ([acc x]
+         (vreset! last x)
+         acc)
+        ([acc x f]
+         (if (not= f identity)
+           (let [acc (if @last (rf acc @last identity) acc)
+                 acc (reduce (fn [acc x]
+                               (rf acc x identity)) acc @buffer)]
+             (vreset! buffer [])
+             (vreset! last x)
+             (vreset! fx f)
+             acc)
+           (do
+             (vswap! buffer conj x)
+             acc)))))))
--- test/ductors/core_test.clj
+++ test/ductors/core_test.clj
@@ -27,37 +27,6 @@
          (sut/get (comp (sut/in [:a]) sut/all)
                   {:a [1 2 3] :b [2 3 4]}))))
 
-(deftest get-duct
-  (is (= '(1 3 5 7 9)
-         (sut/get (sut/duct (sut/filter odd?)) (range 10))))
-  (is (= ['(0 2) '(0 2 4)]
-         (sut/get (comp (sut/duct (sut/filter (comp odd? count)))
-                        (sut/duct (sut/filter even?)))
-                  [(range 2) (range 3) (range 4) (range 5)]))))
-
-(deftest lens-xforms-are-compatible
-  (is (= '(1 3 5 7 9)
-         (sequence (sut/filter odd?) (range 10))))
-  (is (= '(1 2 3 4 5 6 7 8 9 10)
-         (sequence (sut/map inc) (range 10)))))
-
-(deftest update-duct
-  (is (= '(0 2 2 4 4 6 6 8 8 10)
-         (sut/update (sut/duct (sut/filter odd?)) inc (range 10))))
-  (is (= ['(0 1) '(1 1 3) '(0 1 2 3) '(1 1 3 3 5)]
-         (sut/update (comp (sut/duct (sut/filter (comp odd? count)))
-                           (sut/duct (sut/filter even?)))
-                     inc
-                     [(range 2) (range 3) (range 4) (range 5)]))))
-
-(deftest map-duct
-  (is (= [2 3 4 5 6]
-         (sut/get (sut/duct (sut/map inc)) [1 2 3 4 5])))
-  (is (= [3 4 5 6 7]
-         (sut/update (sut/duct (sut/map inc)) inc [1 2 3 4 5])))
-  (is (= [1 1 1 1 1]
-         (sut/assoc (sut/duct (sut/map inc)) 1 [1 2 3 4 5]))))
-
 (deftest nth
   (is (= 0 (sut/get sut/first [0 1 2])))
   (is (= 1 (sut/get sut/second [0 1 2])))
@@ -66,12 +35,3 @@
   (is (= [2 1 2] (sut/assoc sut/first 2 [0 1 2])))
   (is (= 2 (sut/get sut/last [0 1 2])))
   (is (= [0 1 0] (sut/assoc sut/last 0 [0 1 2]))))
-
-(deftest take
-  (is (= '(0 1 2) (sut/get (sut/duct (sut/take 3)) (range 10))))
-  (is (= '(1 2 3 3 4 5 6 7 8 9) (sut/update (sut/duct (sut/take 3)) inc (range 10))))
-  (is (= '(0 0 0 3 4 5 6 7 8 9) (sut/assoc (sut/duct (sut/take 3)) 0 (range 10)))))
-
-(deftest duct-composition
-  (is (= '(0 42 2 42 4 42 6 7 8 9) (sut/assoc (sut/duct (comp (sut/filter odd?) (sut/take 3))) 42 (range 10))))
-  (is (= '(0 42 2 3 4 5 6 7 8 9) (sut/assoc (sut/duct (comp (sut/take 3) (sut/filter odd?))) 42 (range 10)))))
